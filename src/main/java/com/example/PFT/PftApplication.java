4e7974241d4621c40203c01f20543842c246571a
yara1611 <76455290+yara1611@users.noreply.github.com>
2025-02-07 21:38:59 +0200

    Initial commit

---
+++ src/main/java/com/example/PFT/PftApplication.java
@@ -0,0 +1,13 @@
+package com.example.PFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PftApplication.class, args);
+	}
+
+}
