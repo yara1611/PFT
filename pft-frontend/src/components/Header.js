2fe0196ae7343deb625f3d3e373676141bf638b4
yara1611 <76455290+yara1611@users.noreply.github.com>
2025-07-17 01:43:27 +0300

    log in flow

--- pft-frontend/src/components/Header.js
+++ pft-frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import NavMenu from "./NavMenu";
-import { useNavigate,useState } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 //header returns to loggedout state when refreshed
 //make an avatar component
